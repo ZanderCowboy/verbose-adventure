2ededad35659f084261070040e4f5245630e084d
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-10 00:37:48 +0200

    Added new file Constants.py containing all constants used throughout code.

---
+++ engine/Components/Constants.py
@@ -0,0 +1,11 @@
+# Global Variables
+# todo Change to UPPER CASE, for constants
+left_brackets = "({["
+right_brackets = ")}]"
+binary_connectives = ['^', 'v', '->', '<->']
+unary_connectives = ['!']
+
+LEFT_BRACKETS = "({["
+RIGHT_BRACKETS = ")}]"
+BINARY_CONNECTIVES = ['^', 'v', '->', '<->']
+UNARY_CONNECTIVES = ['!']
