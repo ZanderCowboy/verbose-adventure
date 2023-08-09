c0df9976c7dad375860d6e8e8f1f20f28878df2d
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-10 00:36:55 +0200

    Cleaned up code.

--- engine/Simulator/UserInput.py
+++ engine/Simulator/UserInput.py
@@ -1,13 +1,8 @@
-from Parser.Parse.ParseFunction import parse_statements
-from Simulator.input_statements import statement
 from Parser.Parse.AnalizeFunction import analize_statement
-from Components.Sanitizer import sanitize_statements
+from Parser.Parse.ParseFunction import parse_array
 
 
-# returns an integer of the number of variables,
-# an array and string of the variables respectively,
-# a statement that is provided and an array with the individual elements
-def user_input():
+def user_input(statement):
     """
     Takes a given statement, analyzes it and returns the elements and variables.
     :return: returns an integer of the number of variables, an array and string
@@ -16,31 +11,18 @@ def user_input():
     """
     # todo add console output with table of connectives in formal logic
 
-    # print("In user_input():")
     # statement = input("Please enter the statement(s): ")
+
     print("Please enter the statement(s): " + statement)
 
     # analyze statement to determine the number of variables,
     # and split into an array of elements
     number_of_variables, variables_arr, variables_str, elements = analize_statement(statement)
 
-    # add section to sanitize user input
-    sanitized_statement = ""
-    sanitized_statement = sanitize_statements(statement)
-
-
-    # add section to verify statement returned from Sanitizer.py
-
-
-
-    # add section to handles errors if the Verifier fails
-
-
-
+    # todo add section to verify statement returned from Sanitizer.py
+    # todo add section to handles errors if the Verifier fails
 
     # Combine variables and connectives
-    # print("\nCalling parse_statements():")
-    new_elements = parse_statements(statement, elements, variables_arr)
+    new_elements = parse_array(elements, variables_arr)
 
-    # print("Out user_input():")
     return number_of_variables, variables_arr, variables_str, statement, new_elements
