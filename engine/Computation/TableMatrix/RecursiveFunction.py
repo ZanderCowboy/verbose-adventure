646aed38ad27a40a49ef203b59556afc55a30f34
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-10 00:53:39 +0200

    Refactored code and added constants

--- engine/Computation/TableMatrix/RecursiveFunction.py
+++ engine/Computation/TableMatrix/RecursiveFunction.py
@@ -1,4 +1,4 @@
-# Creates a table of T's and F's recursively
+from Components.Constants import TRUE, FALSE
 
 
 def recursive_table(n):
@@ -8,13 +8,12 @@ def recursive_table(n):
 	:param n: Number of variables
 	:return: Returns an array table with T/F values
 	"""
-	# array_table = []
+	array_table = []
 	if n == 1:
-		array_table = ['T', 'F']
-	# array_table = []
+		array_table = [TRUE, FALSE]
 	elif n != 1:
 		array_table = recursive_table(n - 1)
-		temp_table = []
+		# temp_table = []
 		temp_table = array_table.copy()
 		for j in range(0, int(2 ** n / 2)):
 			if j % 2 == 0:  # even
@@ -25,9 +24,8 @@ def recursive_table(n):
 	if n > 1:
 		for i in range(0, 2 ** n):
 			if i % 2 == 0:  # even
-				array_table[i] = str(array_table[i]) + 'T'
+				array_table[i] = str(array_table[i]) + TRUE
 			elif i % 2 == 1:  # odd
-				array_table[i] = str(array_table[i]) + 'F'
+				array_table[i] = str(array_table[i]) + FALSE
 
-	# print(recursive_table(n))
 	return array_table
