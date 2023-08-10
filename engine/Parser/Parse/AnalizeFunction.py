24939cc65f64001469a7bdb781ef0c7f145c9e5b
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-10 12:44:28 +0200

    Refactored code for AnalizeFunction.py

--- engine/Parser/Parse/AnalizeFunction.py
+++ engine/Parser/Parse/AnalizeFunction.py
@@ -1,33 +1,25 @@
-# Analize statement, extracts all the relevant information and variables
-from Components.FindVariables import find_variables
-from Components.Sanitizer import clean_whitespaces
-from Components.Sanitizer import clean_commas
 from Components.CreateConditionals import create_conditionals
+from Components.CreateElements import create_array_of_elements
+from Components.FindVariables import find_variables
+from Components.Sanitizer import clean_commas
+from Components.Sanitizer import clean_whitespaces
 
 
-# returns
 def analize_statement(statement):
     """
     Analyzes statement to determine the number of variables, and split into an array of elements
-    :param statement:
-    :return: length of array of variables as int, array of variables, string of variables, and array with elements
+    :param statement: string
+    :returns: length of array of variables as int, array of variables, string of variables, and array with elements
     """
-
-    # print("***********************************************************")
-    # print("In analize_statement():")
-    # print("given statement=" + statement)
 
     statement = clean_whitespaces(statement)
     statement = clean_commas(statement)
 
     # find variables
     variables_arr, variables_str = find_variables(statement)
-    # print("variables=" + str(variables_arr))
-    # print("variables_str=" + variables_str)
 
     # sterilize statement into elements
-    elements = create_conditionals(statement)
+    array_of_elements: list = create_array_of_elements(statement)
+    array_of_elements = create_conditionals(array_of_elements)
 
-    # print("Out analyze_statement():")
-    return len(variables_arr), variables_arr, variables_str, elements
-
+    return len(variables_arr), variables_arr, array_of_elements
