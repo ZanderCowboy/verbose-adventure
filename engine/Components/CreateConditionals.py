e38b79282532e811f88e579a35f7fd2b3d887ddd
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-10 12:46:43 +0200

    In CreateConditionals.py, extracted the array creation into a new file CreateElements.py and refactored code.

--- engine/Components/CreateConditionals.py
+++ engine/Components/CreateConditionals.py
@@ -1,36 +1,46 @@
-
-
-def create_conditionals(statement):
+def create_conditionals(array_of_elements: list) -> list:
 	"""
 	Creates the conditional -> from ['-', '>'] and
 	biconditional <-> from ['<', '-', '>'].
 
-	:param statement: takes a statement with individual pieces in an array.
+	:rtype: list
+	:param array_of_elements: takes a statement with individual pieces in an array.
 	:return: Returns a new array with connectives as one element.
 	"""
 
-	elements = []
+	# elements = []
 	conditional_symbol = False
 	biconditional_symbol = False
-	for i in range(len(statement)):
-		char = statement[i]
-		if char == '<':
-			if statement[i + 1] == '-':
-				if statement[i + 2] == '>':
-					elements.append("<->")
-					biconditional_symbol = True
+	for i in range(len(array_of_elements)):
+		if i == len(array_of_elements):
+			break
+		char = array_of_elements[i]
+		if char == '<':  # checks to see if we have a biconditional
+			if array_of_elements[i + 1] == '-':
+				if array_of_elements[i + 2] == '>':
+					# elements.append("<->")
+					# biconditional_symbol = True
+					array_of_elements.pop(i)
+					array_of_elements.pop(i)
+					array_of_elements.pop(i)
+					array_of_elements.insert(i, "<->")
 			continue
-		elif char == '-' and biconditional_symbol is False:
-			if statement[i + 1] == '>':
-				elements.append("->")
-				conditional_symbol = True
+
+		elif char == '-' and biconditional_symbol is False:  # if we have a conditional
+			if array_of_elements[i + 1] == '>':
+				# elements.append("->")
+				# conditional_symbol = True
+				array_of_elements.pop(i)
+				array_of_elements.pop(i)
+				array_of_elements.insert(i, "->")
 				continue
 		elif char == '>' and conditional_symbol is False:
 			continue
-		else:
+		else:  # other characters
 			if conditional_symbol is False and biconditional_symbol is False:
-				elements.append(char)
+				# elements.append(char)
+				pass
 			conditional_symbol = False
 			biconditional_symbol = False
 
-	return elements
+	return array_of_elements
