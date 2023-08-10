6c9ee806c6953126905a5e17665a26f8a883e017
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-10 14:47:11 +0200

    In Constants.py, added more constant variables.

--- engine/Components/Constants.py
+++ engine/Components/Constants.py
@@ -9,3 +9,25 @@ LEFT_BRACKETS = "({["
 RIGHT_BRACKETS = ")}]"
 BINARY_CONNECTIVES = ['^', 'v', '->', '<->']
 UNARY_CONNECTIVES = ['!']
+
+TRUE = 'T'
+FALSE = 'F'
+
+# todo Add other connectives such as &&, ||, etc.
+VALID_BRACKETS = "(){}[]"
+VALID_CONNECTIVES = "^v!<->"
+
+WHITE_SPACES = " "
+
+# todo What are illegal characters
+# What are illegal characters
+# numbers
+# (0 ^ 1)
+# (a ^ r)
+# (a_1 v a_3)
+
+# Negation:		! ~ ¬(Alt 170)
+# Conjunction:	^ & &&
+# Disjunction:	v | ||
+# Conditional:	-> => →(Alt 26)
+# Biconditional:	<-> <=>
