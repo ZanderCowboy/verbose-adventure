2d064c29fe9d8a377467c8c4b0a8f01f0e90465d
ZanderCowboy <zanderkotze99@gmail.com>
2023-08-15 23:17:53 +0200

    Added page_measurements.dart to have all measurement constants

---
+++ frontend/lib/page_measurements.dart
@@ -0,0 +1,15 @@
+// First Layer
+
+
+// Second Layer
+const double sectionOneWidth = 0.2;
+const double sectionOneHeight = 1.0;
+const double sideBarWidth = 0.2;
+const double sideBarHeight = 1.0;
+
+const double sectionTwoWidth = 0.8;
+const double sectionTwoHeight = 1.0;
+const double bodyWidth = 0.8;
+const double bodyHeight = 0.9;
+const double itemsWidth = 0.8;
+const double itemsHeight = 0.1;
