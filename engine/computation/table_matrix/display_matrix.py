384174d5620fae9c2e240332e3965915ff1ba4f8
ZanderCowboy <zanderkotze99@gmail.com>
2023-10-11 20:42:37 +0200

    Fixed linting issues.

--- engine/computation/table_matrix/display_matrix.py
+++ engine/computation/table_matrix/display_matrix.py
@@ -3,110 +3,117 @@ from engine_logging.logging_config import logger
 
 
 def display_matrix(number_of_variables: int, returned_matrix: list, all_variables: list) -> None:
-	logger.debug("Displaying matrix to console...")
+    """
 
-	auxiliary_variables = []  # Holds all the placeholder variables
-	for i in range(number_of_variables, len(all_variables) - 1):
-		auxiliary_variables.append(all_variables[i])
-	number_of_aux_variables = len(auxiliary_variables)
+    :param number_of_variables:
+    :param returned_matrix:
+    :param all_variables:
+    :return:
+    """
+    logger.debug("Displaying matrix to console...")
 
-	# prints part for variables
-	print("| ", end='')
-	for j in range(number_of_variables):
-		if j != number_of_variables-1:
-			print(all_variables[j] + " | ", end='')
-		else:
-			print(all_variables[j] + " || ", end='')
+    auxiliary_variables = []  # Holds all the placeholder variables
+    for i in range(number_of_variables, len(all_variables) - 1):
+        auxiliary_variables.append(all_variables[i])
+    number_of_aux_variables = len(auxiliary_variables)
 
-	# prints auxiliary variables
-	index_between_auxiliary_variables_and_last = number_of_variables + len(auxiliary_variables)
-	for j in range(number_of_variables, index_between_auxiliary_variables_and_last):
-		if j != index_between_auxiliary_variables_and_last - 1:
-			print(all_variables[j] + " | ", end='')
-		else:
-			print(all_variables[j] + " || ", end='')
+    # prints part for variables
+    print("| ", end='')
+    for j in range(number_of_variables):
+        if j != number_of_variables-1:
+            print(all_variables[j] + " | ", end='')
+        else:
+            print(all_variables[j] + " || ", end='')
 
-	# prints last column
-	print(all_variables[-1] + " || ", end='')
+    # prints auxiliary variables
+    index_between_auxiliary_variables_and_last = number_of_variables + len(auxiliary_variables)
+    for j in range(number_of_variables, index_between_auxiliary_variables_and_last):
+        if j != index_between_auxiliary_variables_and_last - 1:
+            print(all_variables[j] + " | ", end='')
+        else:
+            print(all_variables[j] + " || ", end='')
 
-	print('\n', end='')
+    # prints last column
+    print(all_variables[-1] + " || ", end='')
 
-	count_characters = get_number_of_spaces(number_of_variables, number_of_aux_variables, all_variables)
-	print("=" * count_characters)
+    print('\n', end='')
 
-	# prints the T/F values for each row in matrix
-	for i in range(len(returned_matrix)):
-		row = returned_matrix[i]
+    count_characters = get_number_of_spaces(number_of_variables, number_of_aux_variables, all_variables)
+    print("=" * count_characters)
 
-		print("| ", end='')
+    # prints the T/F values for each row in matrix
+    for i in range(len(returned_matrix)):
+        row = returned_matrix[i]
 
-		# prints part for variables
-		for j in range(number_of_variables):
-			if j != number_of_variables-1:
-				print(row[j] + " | ", end='')
-			else:
-				print(row[j] + " || ", end='')
+        print("| ", end='')
 
-		# prints auxiliary variables
-		index_between_auxiliary_variables_and_last = number_of_variables + len(auxiliary_variables)
-		for j in range(number_of_variables, index_between_auxiliary_variables_and_last):
-			if j != index_between_auxiliary_variables_and_last - 1:
-				print(row[j] + " | ", end='')
-			else:
-				temp_length = len(all_variables[j])
-				add_len = 0
-				if temp_length % 2 == 1:  # odd
-					add_len = temp_length/2 + 1
-				add_space = ''
-				for k in range(int(add_len)):
-					add_space += ' '
-				print(row[j] + add_space + " || ", end='')
+        # prints part for variables
+        for j in range(number_of_variables):
+            if j != number_of_variables-1:
+                print(row[j] + " | ", end='')
+            else:
+                print(row[j] + " || ", end='')
 
-		# prints last column
-		temp_length = len(all_variables[-1])
-		add_len = 0
-		if temp_length % 2 == 1:  # odd
-			add_len = temp_length/2 + 1
-		elif temp_length % 2 == 0:  # even
-			add_len = temp_length/2
-		add_space = ''
-		for k in range(int(add_len)):
-			add_space += ' '
-		print(row[-1] + add_space + " || ", end='')
-		print('\n', end='')
+        # prints auxiliary variables
+        index_between_auxiliary_variables_and_last = number_of_variables + len(auxiliary_variables)
+        for j in range(number_of_variables, index_between_auxiliary_variables_and_last):
+            if j != index_between_auxiliary_variables_and_last - 1:
+                print(row[j] + " | ", end='')
+            else:
+                temp_length = len(all_variables[j])
+                add_len = 0
+                if temp_length % 2 == 1:  # odd
+                    add_len = temp_length/2 + 1
+                add_space = ''
+                for k in range(int(add_len)):
+                    add_space += ' '
+                print(row[j] + add_space + " || ", end='')
 
-	print("=" * count_characters)
+        # prints last column
+        temp_length = len(all_variables[-1])
+        add_len = 0
+        if temp_length % 2 == 1:  # odd
+            add_len = temp_length/2 + 1
+        elif temp_length % 2 == 0:  # even
+            add_len = temp_length/2
+        add_space = ''
+        for k in range(int(add_len)):
+            add_space += ' '
+        print(row[-1] + add_space + " || ", end='')
+        print('\n', end='')
+
+    print("=" * count_characters)
 
 
 # todo Finish Function
 def get_number_of_spaces(number_of_variables: int, nr_auxiliary_variables: int, all_variables: list) -> int:
-	logger.debug("Calling get_number_of_spaces(%d, %d, %s)...", number_of_variables, nr_auxiliary_variables, all_variables)
+    logger.debug("Calling get_number_of_spaces(%d, %d, %s)...", number_of_variables, nr_auxiliary_variables, all_variables)
 
-	space = 3
-	total_space = 0
+    space = 3
+    total_space = 0
 
-	initial_space = 1
-	total_space += initial_space
-	spaces_for_variables = space * number_of_variables
-	total_space += spaces_for_variables
-	spaces_for_single_sep = number_of_variables - 1
-	total_space += spaces_for_single_sep
+    initial_space = 1
+    total_space += initial_space
+    spaces_for_variables = space * number_of_variables
+    total_space += spaces_for_variables
+    spaces_for_single_sep = number_of_variables - 1
+    total_space += spaces_for_single_sep
 
-	length_of_aux_strings = 0
-	for i in range(number_of_variables, len(all_variables) - 1):
-		temp_length = len(all_variables[i])
-		length_of_aux_strings += temp_length
-	length_of_aux_strings += nr_auxiliary_variables * 2
-	spaces_for_aux_variables = length_of_aux_strings
-	total_space += spaces_for_aux_variables
-	spaces_for_single_sep = nr_auxiliary_variables - 1
-	total_space += spaces_for_single_sep
+    length_of_aux_strings = 0
+    for i in range(number_of_variables, len(all_variables) - 1):
+        temp_length = len(all_variables[i])
+        length_of_aux_strings += temp_length
+    length_of_aux_strings += nr_auxiliary_variables * 2
+    spaces_for_aux_variables = length_of_aux_strings
+    total_space += spaces_for_aux_variables
+    spaces_for_single_sep = nr_auxiliary_variables - 1
+    total_space += spaces_for_single_sep
 
-	space_for_last_variable = len(all_variables[-1])
-	total_space += space_for_last_variable
-	spaces_for_double_sep = 6
-	total_space += spaces_for_double_sep
+    space_for_last_variable = len(all_variables[-1])
+    total_space += space_for_last_variable
+    spaces_for_double_sep = 6
+    total_space += spaces_for_double_sep
 
-	logger.debug("total_space=%d", total_space)
-	logger.debug("Finished calculating the total space.")
-	return total_space
+    logger.debug("total_space=%d", total_space)
+    logger.debug("Finished calculating the total space.")
+    return total_space
--- engine/computation/table_matrix/recursive_table.py
+++ engine/computation/table_matrix/recursive_table.py
@@ -4,38 +4,38 @@ from engine_logging.logging_config import logger
 
 
 def recursive_table(n):
-	"""
-	Creates a list of T's and F's recursively, given the number of variables
-	For example:
-	recursive_table(1) => ['T', 'F'] and
-	recursive_table(3) =>
-	['TTT', 'TTF', 'TFT', 'TFF', 'FTT', 'FTF', 'FFT', 'FFF']
+    """
+    Creates a list of T's and F's recursively, given the number of variables
+    For example:
+    recursive_table(1) => ['T', 'F'] and
+    recursive_table(3) =>
+    ['TTT', 'TTF', 'TFT', 'TFF', 'FTT', 'FTF', 'FFT', 'FFF']
 
-	:param n: (int) Number of variables
-	:return: Returns an array table with T/F values
-	"""
-	logger.debug("Creating table entries recursively...")
+    :param n: (int) Number of variables
+    :return: Returns an array table with T/F values
+    """
+    logger.debug("Creating table entries recursively...")
 
-	logger.debug("n=%d", n)
-	array_table = []
-	if n == 1:
-		array_table = [TRUE, FALSE]
-	elif n != 1:
-		array_table = recursive_table(n - 1)
-		temp_table = array_table.copy()
-		for j in range(0, int(2 ** n / 2)):
-			if j % 2 == 0:  # even
-				array_table.insert(j * 2, temp_table[j])
-			elif j % 2 == 1:  # odd
-				array_table.insert(j * 2, temp_table[j])
+    logger.debug("n=%d", n)
+    array_table = []
+    if n == 1:
+        array_table = [TRUE, FALSE]
+    elif n != 1:
+        array_table = recursive_table(n - 1)
+        temp_table = array_table.copy()
+        for j in range(0, int(2 ** n / 2)):
+            if j % 2 == 0:  # even
+                array_table.insert(j * 2, temp_table[j])
+            elif j % 2 == 1:  # odd
+                array_table.insert(j * 2, temp_table[j])
 
-	if n > 1:
-		for i in range(0, 2 ** n):
-			if i % 2 == 0:  # even
-				array_table[i] = str(array_table[i]) + TRUE
-			elif i % 2 == 1:  # odd
-				array_table[i] = str(array_table[i]) + FALSE
+    if n > 1:
+        for i in range(0, 2 ** n):
+            if i % 2 == 0:  # even
+                array_table[i] = str(array_table[i]) + TRUE
+            elif i % 2 == 1:  # odd
+                array_table[i] = str(array_table[i]) + FALSE
 
-	logger.debug("array_table=%s", array_table)
-	logger.debug("Finished creating array of table entries.")
-	return array_table
+    logger.debug("array_table=%s", array_table)
+    logger.debug("Finished creating array of table entries.")
+    return array_table
